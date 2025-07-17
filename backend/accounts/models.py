e90634984ea20a94c56522a2305941253fd96d42
Steve Costin <steve@thecostins.uk>
2025-07-17 01:10:47 +0100

    Initial commit

---
+++ backend/accounts/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, first_name, last_name, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email, password, first_name, last_name, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', False)
+
+        return self._create_user(email, password, first_name, last_name, **extra_fields)
+
+    def create_superuser(self, email, password, first_name, last_name, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self._create_user(email, password, first_name, last_name, **extra_fields)
+
+# Create your User model here.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    is_staff = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
