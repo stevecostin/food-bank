c6eaa4eddc737c6eef7a9ad360e3dce6b2834d6d
Steve Costin <steve@thecostins.uk>
2025-07-27 20:38:14 +0100

    Added routes for Dashboard, Clients, Staff

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Dashboard from '@/views/Dashboard.vue';
+import Clients from '@/views/Clients.vue';
+import Staff from '@/views/Staff.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'dashboard',
+      component: Dashboard,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/clients',
+      name: 'clients',
+      component: Clients,
+    },
+    {
+      path: '/staff',
+      name: 'staff',
+      component: Staff,
     },
   ],
-})
+});
 
-export default router
+export default router;
