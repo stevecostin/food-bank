73ebd2892dcf39fcb83921960a467f1b969212c6
Steve Costin <steve@thecostins.uk>
2025-07-27 21:36:31 +0100

    Allow changing of the page title depending on the route selected

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,8 @@ import Dashboard from '@/views/Dashboard.vue';
 import Clients from '@/views/Clients.vue';
 import Staff from '@/views/Staff.vue';
 
+const siteName = 'Food Bank';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -10,18 +12,37 @@ const router = createRouter({
       path: '/',
       name: 'dashboard',
       component: Dashboard,
+      meta: {
+        title: 'Dashboard',
+      },
     },
     {
       path: '/clients',
       name: 'clients',
       component: Clients,
+      meta: {
+        title: 'Clients',
+      },
     },
     {
       path: '/staff',
       name: 'staff',
       component: Staff,
+      meta: {
+        title: 'Staff',
+      },
     },
   ],
 });
+
+router.beforeEach((to, from) => {
+  let title = '';
+
+  if (to.meta.title) {
+    title = ` - ${to.meta.title}`
+  }
+
+  document.title = `${siteName}${title}`;
+})
 
 export default router;
