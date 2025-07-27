ef61fba72c21d5c4b9ac786693fb1f2eac5c7500
Steve Costin <steve@thecostins.uk>
2025-07-27 20:37:42 +0100

    Added PrimeVue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,52 @@
-<template>
-  <RouterView />
-</template>
-
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from "vue";
+import Menubar from 'primevue/menubar';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const items = ref([
+    {
+        label: 'Dashboard',
+		command: () => {
+			router.push('/');
+		}
+    },
+    {
+        label: 'People',
+        items: [
+            {
+                label: 'Clients',
+				command: () => {
+					router.push('/clients');
+				}
+            },
+            {
+                label: 'Staff',
+				command: () => {
+					router.push('/staff');
+				}
+            },
+        ]
+    },
+    {
+        label: 'Stock',
+		command: () => {
+
+		}
+    },
+]);
 </script>
+
+<template>
+<div>
+	<Menubar :model="items" />
+</div>
+<div>
+	<RouterView />
+</div>
+</template>
 
 <style scoped>
 
