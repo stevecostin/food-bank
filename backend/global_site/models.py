514d1337d002c9f4b00b13c78a32153e1da8b153
Steve Costin <steve@thecostins.uk>
2025-07-30 21:58:41 +0100

    Added SingletonModel

--- backend/global_site/models.py
+++ backend/global_site/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
-class GlobalConfiguration(models.Model):
+class SingletonModel(models.Model) :
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(SingletonModel, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
+
+class GlobalConfiguration(SingletonModel):
     class Meta:
         db_table = 'global_configuration'
 
--- backend/global_site/views.py
+++ backend/global_site/views.py
@@ -2,7 +2,7 @@ from django.http.response import JsonResponse
 from .models import GlobalConfiguration
 
 def get_global_configuration(request):
-    global_config = GlobalConfiguration()
+    global_config = GlobalConfiguration().load()
 
     return JsonResponse({
         'site-name': global_config.name
