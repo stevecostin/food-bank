9777e84ce8298d598f958dbca5c84f384d5b4398
Steve Costin <steve@thecostins.uk>
2025-07-31 23:39:19 +0100

    Added ToastService and importing primeicons.css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,11 @@
 import './assets/main.css';
+import 'primeicons/primeicons.css'
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
+import ToastService from 'primevue/toastservice';
 
 const app = createApp(App);
 
@@ -13,5 +15,6 @@ app.use(PrimeVue, {
         preset: Aura,
     }
 });
+app.use(ToastService);
 
 app.mount('#app');
