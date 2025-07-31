725f387523ce3afcc7fcaffa6a39cc5ad3225228
Steve Costin <steve@thecostins.uk>
2025-07-31 01:39:08 +0100

    Using PostgreSQL

--- backend/main/settings/development.py
+++ backend/main/settings/development.py
@@ -1,3 +1,13 @@
 from .base import *
 
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'food-bank',
+        'USER': os.environ.get('DATABASE_USER'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+    }
+}
--- backend/main/settings/production.py
+++ backend/main/settings/production.py
@@ -4,3 +4,14 @@ from .base import *
 os.environ["DJANGO_RUNSERVER_HIDE_WARNING"] = "true"
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'food-bank',
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+    }
+}
