696d65b5282bfd70f560594fa7c8f662d4d08553
Steve Costin <steve@thecostins.uk>
2025-10-15 00:47:20 +0100

    Added pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,17 @@
 import './assets/main.css';
 import 'primeicons/primeicons.css'
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import ToastService from 'primevue/toastservice';
-import {enUK} from './locales';
+import { enUK } from './locales';
+import axios from "axios";
 
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(router);
 app.use(PrimeVue, {
@@ -18,5 +21,8 @@ app.use(PrimeVue, {
     locale: enUK,
 });
 app.use(ToastService);
+app.use(pinia);
+
+axios.defaults.baseURL = 'http://localhost:8000'
 
 app.mount('#app');
