231e85a04e5c44e249aaf21f3de969d19b3103e5
Steve Costin <steve@thecostins.uk>
2025-10-16 00:53:50 +0100

    Updated endpoints

---
+++ backend/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import GetCSRFToken, LoginView, LogoutView, CheckAuthenticatedView
+
+urlpatterns = [
+    path('csrf_cookie', GetCSRFToken.as_view()),
+    path('login', LoginView.as_view()),
+    path('logout', LogoutView.as_view()),
+    path('authenticated', CheckAuthenticatedView.as_view())
+]
--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,16 +1,24 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # Admin portal
     path('admin/', admin.site.urls),
 
+    path('api-auth/', include('rest_framework.urls')),
+
+    # Accounts
+    path('accounts/', include('accounts.urls')),
+
     # Global configuration
-    path('', include('global_site.urls')),
+    path('global/', include('global_site.urls')),
 
     # Frontend API's
-    path('', include('frontend_api.urls')),
+    path('api/', include('frontend_api.urls')),
 
     # Core
-    path('', include('core.urls')),
+    path('core/', include('core.urls')),
 ]
+
+# urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
