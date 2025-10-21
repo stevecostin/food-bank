2f1f8fc14b05ea6c71a6b97a950a1f9126a8072f
Steve Costin <steve@thecostins.uk>
2025-10-21 21:55:01 +0100

    Added ability to login

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { Button, InputText, Password, useToast } from "primevue";
+import { ref } from 'vue';
+import { useAuthStore } from "@/stores/auth.js";
+import { useRouter, useRoute } from 'vue-router'
+import { Form } from '@primevue/forms';
+import api from "@/components/api.js";
+import { useUtils } from "@/composables/utils.js";
+
+const router = useRouter();
+const route = useRoute();
+const toast = useToast();
+
+const email = ref(null);
+const password = ref(null);
+
+const authStore = useAuthStore();
+
+const onFormSubmit = async ({ valid }) => {
+	const formData = {
+		email: email.value,
+		password: password.value,
+	};
+
+	const headers = {
+		headers: {
+			'X-CSRFToken': useUtils().getCookie('csrftoken'),
+			'Content-Type': 'application/json',
+			'Accept': 'application/json',
+		}
+	}
+
+	try {
+		let response = await api.post('/accounts/login', formData, headers);
+
+		const token = response.data;
+
+		if (token.success) {
+			authStore.login();
+		}
+
+		await router.push(route.query.to || '/');
+
+		toast.add({ severity: 'success', summary: 'Form is submitted.', life: 3000 });
+	} catch (e) {
+		console.error(e);
+	}
+};
+</script>
+
+<template>
+<Form v-slot="$form" @submit="onFormSubmit" class="flex flex-col gap-3 w-full sm:w-64">
+	<div class="card flex">
+        <div class="flex flex-col gap-1">
+			<label for="email">Email</label>
+			<InputText type="text" v-model="email" id="email" variant="filled" />
+		</div>
+	</div>
+	<div class="card flex">
+        <div class="flex flex-col gap-1">
+			<label for="password">Password</label>
+			<Password v-model="password" :feedback="false" id="password" variant="filled" toggleMask />
+		</div>
+	</div>
+	<div class="card flex">
+        <div class="flex flex-col gap-1">
+			<Button type="submit" severity="secondary" label="Login" />
+		</div>
+	</div>
+</Form>
+</template>
+
+<style scoped>
+
+</style>
