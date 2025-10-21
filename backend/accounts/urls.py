3b91e9b8acfec8862e34ed682eb67649a2a95434
Steve Costin <steve@thecostins.uk>
2025-10-21 15:44:50 +0100

    Changed csrf_cookie to csrf_token

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import GetCSRFToken, LoginView, LogoutView, CheckAuthenticatedView
 
 urlpatterns = [
-    path('csrf_cookie', GetCSRFToken.as_view()),
+    path('csrf_token', GetCSRFToken.as_view()),
     path('login', LoginView.as_view()),
     path('logout', LogoutView.as_view()),
     path('authenticated', CheckAuthenticatedView.as_view())
