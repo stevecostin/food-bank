36d463fcf411fef0837cfea804cb9e7d4e560dff
Steve Costin <steve@thecostins.uk>
2025-10-22 16:15:47 +0100

    Added csrf to every header

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,8 +1,18 @@
 import axios from 'axios';
+import { useUtils } from "@/composables/utils.js";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true,
 });
+
+api.interceptors.request.use(
+    (config) => {
+        config.headers['X-CSRFToken'] = useUtils().getCookie('csrftoken');
+        config.headers['Content-Type'] = 'application/json';
+
+        return config;
+    }
+);
 
 export default api;
